ae07b3d14f05ec0155916fd6d0079cd6a33c5550
sivna2504 <sivan2504@gmail.com>
2020-10-20 16:28:26 +0300

    react:contonue signup

--- app2/src/component/signup.js
+++ app2/src/component/signup.js
@@ -3,7 +3,23 @@ import axios from '../axios'
 import '../style.css';
 
 class Signup extends Component {
-    state = {
+
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         pateint:{
+    //         PatientId: "",
+    //         Firstname: "",
+    //         Lastname: "",
+    //         PatientTz: "",
+    //         DateOfBirth: "",
+    //         Email: "",
+    //         Password: "",
+    //         PhoneNumber1: "",
+    //         PhoneNumber2: ""
+    //     }
+    // }
+   state = {
         patient: {//לא צריך להתשמש במזהה בטופס נכון?
             PatientId: "",
             Firstname: "",
@@ -26,29 +42,35 @@ class Signup extends Component {
 
                     <center><h2>הרשמת מטופל</h2></center><br></br>
 
-
-                    <input type="text" placeholder="שם משפחה" id="Lastname" onChange={(event) => this.inputChange(event)} /> <input type="text" placeholder="שם פרטי" id="Firstname" onChange={(event) => this.inputChange(event)} />  
+                    <center>
+                    <rightto> 
+                    <input type="text" placeholder="שם פרטי" id="Firstname" className="input" onChange={(event) => this.inputChange(event)} /> <input type="text" placeholder="שם משפחה" id="Lastname" className="input" onChange={(event) => this.inputChange(event)} />  
                     <br></br>
                     <br></br>
 
-                   <input type="text" placeholder="ת.ז" id="PatientTz" onChange={(event) => this.inputChange(event)} /> <input type="date" placeholder="תאריך לידה" id="DateOfBirth" onChange={(event) => this.inputChange(event)} />
+                    
+                   <input type="date" placeholder="תאריך לידה" id="DateOfBirth"  className="input" onChange={(event) => this.inputChange(event)} /><input type="text" placeholder="ת.ז" id="PatientTz" className="input" onChange={(event) => this.inputChange(event)} />
                     <br></br>
                     <br></br>
-                    <input type="text"placeholder="מס' פלאפון נוסף (לא חובה)" id="PhoneNumber2" onChange={(event) => this.inputChange(event)} /> <input type="text" placeholder="מספר פלאפון" id="PhoneNumber1" onChange={(event) => this.inputChange(event)} />
+                   
+                    <input type="text" placeholder="מספר פלאפון" id="PhoneNumber1" className="input" onChange={(event) => this.inputChange(event)} /> <input type="text"placeholder="מס' פלאפון נוסף (לא חובה)" id="PhoneNumber2"  className="input"onChange={(event) => this.inputChange(event)} /> 
                     
                     <br></br><br></br>
-                    <input type="text"placeholder="כתובת מייל" id="Email" onChange={(event) => this.inputChange(event)} />
+                    <input type="text"placeholder="כתובת מייל" id="Email"className="input" onChange={(event) => this.inputChange(event)} />
                   
                     <br></br><br></br><br></br>
                     <label>בחר את הסיסמא שתשרת אותך בכניסתך לאתר</label>
                     <br></br>
 
-                    <input type="password"placeholder="אימות סיסמא" id="Valid-Password" /> <input type="password" placeholder="סיסמא" id="Password" onChange={(event) => this.inputChange(event)} />
+                    
+                    <input type="password" className="input" placeholder="סיסמא" id="Password" className="input" onChange={(event) => this.inputChange(event)} />  <input type="password" className="input" placeholder="אימות סיסמא" id="Valid-Password" /> 
                     
                     <br></br><br></br>
 
-                    <input type="button" value="button" onClick={() => this.newPateintHandler(this.state.patient)} />
+                    
                     <center><input type="submit" value="אישור" onClick={() => this.newPateintHandler(this.state.patient)} /></center>
+                    </rightto>
+                    </center>
                 </form>
             </div>
         );
@@ -57,20 +79,28 @@ class Signup extends Component {
    
 
     inputChange = (event) => {
+   
         const newperson = { ...this.state.patient };
         const id = event.target.id;
         newperson[id] = event.target.value;
 
-        this.setState({ pateint: newperson });
-        
+        this.setState({ patient: newperson })
+        // alert(newperson[id])
+    }
+
+    resetValue=(id)=>
+    {
+        const newperson = { ...this.state.patient };
+        newperson[id] = "";
+        this.setState({ patient: newperson })
     }
 
     newPateintHandler = (patient) => {
         debugger;
-        alert(patient.Password.value)
+       
         var flag=1;
         if (document.getElementById("Valid-Password").value != document.getElementById("Password").value){
-            patient.Password = "";
+            this.resetValue("Password");
             document.getElementById("Password").value = "";
             document.getElementById("Valid-Password").value = "";
             flag=0;
