dcc9f7936adc0655f60984baa97fdf64cd981794
chagitmorgenshtern <chagit7322@gmail.com>
2020-10-27 10:31:02 +0200

    react: fix signup

--- app2/src/component/signupTh.js
+++ app2/src/component/signupTh.js
@@ -6,12 +6,10 @@ import { Form, Col, Button, Row, Container, InputGroup } from 'react-bootstrap';
 
 class SignupTh extends Component {
 
-
-
     render() {
         return (
             <div className="signup">
-                <Form >
+                <Form>
                     <Form.Row>
                         <Form.Group as={Col} controlId="FirstName">
                             <Form.Control placeholder="שם פרטי" />
@@ -37,34 +35,35 @@ class SignupTh extends Component {
                         <Form.Group as={Col} controlId="formGridCity">
                             <Form.Label>City</Form.Label>
                             <Form.Control />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="LasttName">
+                            <Form.Control placeholder="שם משפחה" />
 
-                            <Form.Group as={Col} controlId="LasttName">
-                                <Form.Control placeholder="שם משפחה" />
-
-                            </Form.Group>
-                            <Form.Group as={Col} controlId="Id">
-                                <Form.Control placeholder="ת.ז" />
-                            </Form.Group>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="Id">
+                            <Form.Control placeholder="ת.ז" />
+                        </Form.Group>
                     </Form.Row>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="Phone">
-                                <Form.Control placeholder="פלאפון" />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId="Phone2">
-                                <Form.Control placeholder=" פלאפון נוסף (לא חובה)" />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId="BornDate">
-                                <Form.Control type="date" />
-                            </Form.Group>
-                        </Form.Row>
-                        <InputGroup><Form.Control type="email" placeholder="כתובת מייל" /></InputGroup>
-                        {/* <Form.row> </Form.row> */}
-                        <Button variant="primary" type="submit">
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="Phone">
+                            <Form.Control placeholder="פלאפון" />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="Phone2">
+                            <Form.Control placeholder=" פלאפון נוסף (לא חובה)" />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="BornDate">
+                            <Form.Control type="date" />
+                        </Form.Group>
+                    </Form.Row>
+                    <InputGroup><Form.Control type="email" placeholder="כתובת מייל" /></InputGroup>
+                    {/* <Form.row> </Form.row> */}
+                    <Button variant="primary" type="submit">
 
-                            Submit
-  </Button>
-                </Form></div>
-        );
+                        Submit
+                        </Button>
+                </Form>
+            </div>);
+
     }
 
 }
